81c33dbe847b28f679b70234b4344fda759039f4
Alex Feldman <alexfeldman93@gmail.com>
2018-11-07 14:01:40 -0800

    Basic importing file procedure

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+import sys
+
+import click
 
 
-def import_file():
-    pass
+@click.command()
+@click.option('-p', '--path', help='Number of greetings.')
+def import_file(path):
+    if path is None:
+        path = input("Please enter path to Graphml file: ")
+    if not os.path.isfile(path) or os.path.splitext(path)[1] != '.graphml':
+        sys.exit('Incorrect path')
+
 
 
 if __name__ == '__main__':
-    main()
+    import_file()
